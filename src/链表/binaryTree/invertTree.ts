6f4048afc8e1361645d26d8066383169bed7bae8
cl <jcignorance@outlook.com>
2021-02-12 20:03:09 +0800

    feat:反转二叉树

---
+++ src/链表/binaryTree/invertTree.ts
@@ -0,0 +1,14 @@
+import TreeNode from './ds'
+
+function invertTree(root: TreeNode | null): TreeNode | null {
+  if (root === null){
+    return null
+  }
+
+  [root.left, root.right] = [root.right, root.left]
+
+  invertTree(root.left)
+  invertTree(root.right)
+
+  return root
+}
