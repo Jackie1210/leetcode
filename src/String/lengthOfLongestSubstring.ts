6dcf89bb36c149c3383cf164090222a092ae68ed
cl <jcignorance@outlook.com>
2021-02-14 19:08:37 +0800

    feat:无重复字符的最长子串

---
+++ src/String/lengthOfLongestSubstring.ts
@@ -0,0 +1,26 @@
+function lengthOfLongestSubstring(s: string): number {
+  let window = new Map()
+
+  let left = 0
+  let right = 0
+  let res = 0
+  while(right < s.length){
+    const a = s[right]
+    right++
+
+    window.set(a, (window.get(a) || 0) + 1)
+
+    while(window.get(a) > 1){
+      const d = s[left]
+
+      left++
+
+      window.set(d, window.get(d) - 1)
+    }
+
+    res = Math.max(res, right - left)
+
+  }
+
+  return res
+}
