4cdfe57caa4c280a60e2c298ab6f20a5911d3910
cl <jcignorance@outlook.com>
2021-03-28 15:59:06 +0800

    fix:bubblesort

--- src/sort/bubblesort.ts
+++ src/sort/bubblesort.ts
@@ -3,14 +3,20 @@ function bubbleSort(arr: number[]): number[]{
   const n = arr.length
   for(let i = n - 1; i >= 0; i--){
     for(let j = 0; j < i; j++){
-      const temp = arr[i]
-      arr[i] = arr[j]
-      arr[j] = temp
-      flag = true
+      if (arr[j] > arr[j + 1]){
+        swap(arr, i, j)
+        flag = true
+      }
     }
 
     if (!flag) break
   }
 
   return arr
+}
+
+function swap(arr: number[], i: number, j: number): void{
+  const temp = arr[i]
+  arr[i] = arr[j]
+  arr[j] = temp
 }
